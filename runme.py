c83b934c1fcaff84b992a297e20b2bce4b68a9e5
Sean Leeka <sleeka@swbell.net>
2017-06-06 04:35:49 -0500

    Still trying to parse img url's

--- runme.py
+++ runme.py
@@ -10,27 +10,28 @@ class photo_motion(scrapy.Spider):
     with open('conf','r') as f:
 	allowed_domains.append(f.read().strip())
 
-    event_url = allowed_domains[0] + "/groupSessionSelection.php?mode=listSessions&groupID=&eventID="
+    event_url = "http://" + allowed_domains[0] + "/groupSessionSelection.php?mode=listSessions&groupID=&eventID="
 
     start_urls = []
-    for eventID in range(6,17):
+    for eventID in range(6,7):
 	start_urls.append( event_url + str(eventID) )
 
     def parse(self, response):
-	exit()
-	print response.css('div a::attr(href)').extract()[1:-1]
-#	    yield Request(
-#		url=response.url.join(href),
-#		callback=self.parse_race
-#	    )
+	for href in response.css('div a::attr(href)').extract()[1:-1]:
+	    yield Request(
+		url = "http://" + self.allowed_domains[0] + href,
+		callback = self.parse_race
+	    )
 
     def parse_race(self, response):
+	print response.xpath('img/@src').extract()
+#	exit()
 #	for href in response.css('div.download_wrapper a[href$=".img"]::attr(href)').extract():
-	    print href
-            yield Request(
-                url=response.urljoin(href),
-                callback=self.save_img
-            )
+#	    print href
+ #           yield Request(
+  #              url=response.urljoin(href),
+   #             callback=self.save_img
+    #        )
 
     def save_img(self, response, groupID):
         path = response.url.split('/')[-1]
